da7a2f80bdb08b74fe5eb957d5de5cb95c151017
PhoenixRiversHTH <85960558+PhoenixRiversHTH@users.noreply.github.com>
2021-06-17 09:21:13 -0700

    Add files via upload

---
+++ directory_pratice.py
@@ -0,0 +1,9 @@
+
+food = {"Chicken":1.59, "Beef":1.99, "Cheese":1.00, "Milk":2.50}
+Chicken_price = groceries["Chicken"]
+Beef_price = groceries["Beef"]
+Cheese_price = groceries["Cheese"]
+Milk_price = groceries["Milk"]
+NBA_players = {"Lebron James":23, "kevin Durant":35, "Stephen Curry":30, "Damian Lillard":0}
+Steph_jersy = NBA_players["Stephen Curry"]
+print (food[2])
---
+++ hello_world.py
@@ -0,0 +1,15 @@
+print ("hello world again!!!")
+
+bool = True
+pool = True
+cool = True
+tool = False
+
+if(bool):
+    print("hello again")
+elif(pool):
+    print("guess not!")
+elif(cool)
+    print("cool cool!")
+elif(tool):
+    print(tool)
